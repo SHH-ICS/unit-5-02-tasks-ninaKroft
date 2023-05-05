3e2270a0fa8f6a76788fc766f03ebf118d09c326
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-05-05 14:51:00 +0000

    save

--- Q1Length.py
+++ Q1Length.py
@@ -5,10 +5,9 @@ done = False
 
 while done == False:
     word = input("Please enter your word: ")
-    word = len(word)
-    print("The length of your word is", word, "characters.")
-    z = input("Would you like to exit the program? Enter 'quit' for yes or 'continue' for no. ")
+    number = len(word)
+    print("The length of", word, "is", number, "characters.")
+    z = input("Would you like to exit the program? Enter 'quit' for yes. Otherwise, enter anything and the program will run again.")
     if z == 'quit':
         done = True
-    else:
-        print("Please enter 'y' for yes or 'n' for no.")
+    
--- Q2Reverse.py
+++ Q2Reverse.py
@@ -1,1 +1,2 @@
 # Create a program that will accept a word and output the word one letter at a time in reverse.
+
