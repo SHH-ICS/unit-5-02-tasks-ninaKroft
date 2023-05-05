0e76a164b5ae35ed99b1e47cf0062ee193c7eac5
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-05-05 12:32:10 -0400

    Q2 solved

--- Q2Reverse.py
+++ Q2Reverse.py
@@ -1,2 +1,9 @@
 # Create a program that will accept a word and output the word one letter at a time in reverse.
 
+word = input("Please enter your word: ")
+
+number = len(word)
+
+for x in range(1, number + 1):
+    letter = word[-x]
+    print(letter)
